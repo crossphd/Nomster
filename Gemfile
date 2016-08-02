b5fa8412ef68417781f2a264f428275b1fb3d1b0
Chris Ross <chriseross@gmail.com>
2016-08-02 22:12:38 +0000

    added geocoding

--- Gemfile
+++ Gemfile
@@ -44,3 +44,6 @@ gem 'sass-rails'
 gem 'will_paginate'
 gem 'simple_form'
 gem 'devise', '>= 3.2.4'
+gem 'geocoder', '1.3.4'
+gem 'figaro', '>= 1.0.0'
+
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ActiveRecord::Base
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  validates :name, presence: true, length: { minimum: 2, maximum: 40 }
+  validates :address, presence: true, length: { minimum: 5, maximum: 200 }
+  validates :description, presence: true, length: { minimum: 3, maximum: 600 }
 end
